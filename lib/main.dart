7ca63b7085b9170171fa567c12c888c8e16ff459
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-06-26 14:38:17 +0500

    App setup complete

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/material.dart';
+import 'package:weather_app/pages/splash.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp app=MaterialApp()
+    // return app()
+    return const MaterialApp(
+      home: splash(),
+      // home: homepage(),
+      // home: leaderBoard(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+// class my extends StatelessWidget {
+//   const my({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+ // class HomeScreen extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title:  ElevatedButton(
+//             onPressed: (){
+//               print("Button Has been clicked");}
+//             , child: const Text('Button')),
+//       ),
+//     body: const Center(
+//       child: Text("Hello App",
+//           style:TextStyle(fontSize:80, color:Colors.deepPurple )),
+//     )
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
---
+++ lib/pages/splash.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+// import 'package:tictactoe_flutterapp/screens/home.dart';
+
+class splash extends StatelessWidget {
+  const splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Container(
+          margin: const EdgeInsets.only(top: 70),
+          child: Padding(
+            //padding wasnt needed nvm it
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("K A T I   Z E R O",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.w400))
+                  ],
+                ),
+                const SizedBox(
+                  height: 200,
+                ),
+                // GestureDetector(
+                //   onTap: () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(builder: (context) => const homepage()),
+                //     );
+                //   },
+                Image.asset(
+                  'assets/images/Group.png',
+                ),
+                // ),
+                const Spacer(),
+                const Text("P O W E R E D  B Y",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500)),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text("T E C H  I D A R A",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 143, 188, 255),
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600)),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+          ),
+          // child: Image.asset('assets/images/xogrid.png', height: 10),
+        ), // your body content.
+      ),
+    );
+  }
+}
